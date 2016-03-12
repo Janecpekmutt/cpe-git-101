edb229874f7863c1bca499445c4be626eefb62a8
JANE <janejane.jjaannee@gmail.com>
2016-03-12 14:56:33 +0700

    Create multiple function

--- calculator.js
+++ calculator.js
@@ -1,10 +1,14 @@
 function add(x,y){
-	if(isNaN(x)){
-		
-	}
+	if(isNaN(x))return console.log('Input must be number')
 	return x+y;
 }
 
 function sub(x,y){
+	if(isNaN(x))return console.log('Input must be number')
 	return x-y;
+}
+
+function mul(x,y){
+	if(isNaN(x))return console.log('Input must be number')
+	return x*y;
 }
